691f6cb759eb21627efcb5c3c7b2d3e414d01127
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-16 23:34:41 +0100

    installed urql

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -12,16 +12,25 @@ type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   // Declaring properties of the input and their types
   name: string;
   label: string;
-  placeholder: string;
 };
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  // Destructuring size to avoid errors. renaming it to underscore to make explicit that we wont be using it
+  size: _,
+  ...props
+}) => {
   const [field, { error }] = useField(props);
   return (
     // isInvalid takes a boolean so we cast error to boolean
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input
+        {...props}
+        {...field}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
