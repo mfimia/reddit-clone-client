e4e4a71d198b44b6d7a859c32d1cc1a5d70b2019
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-16 23:34:28 +0100

    added urql provider to use client-side graphql queries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,26 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-
+import { Provider, createClient } from "urql";
 import theme from "../theme";
+
+// Creating a graphql client
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    // Providing the client to our whole app
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
   );
 }
 
