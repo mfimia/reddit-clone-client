0587bcdb8542b0f3168d2255cb59c8792cc31c87
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-16 23:33:57 +0100

    set up register mutation query on submit

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -7,18 +7,38 @@ import {
   Box,
 } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
+import { useMutation } from "urql";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
 
 interface RegisterProps {}
 
+// We took this query from graphql playground
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String! ) {
+  register(options: {username: $username, password: $password}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`;
+
 export const Register: React.FC<RegisterProps> = ({}) => {
+  // We don't care about the first option of the hook so we leave it empty
+  // We can now use mutation queries
+  const [, register] = useMutation(REGISTER_MUT);
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(values) => {
-          console.log(values);
+          register(values);
         }}
       >
         {({ isSubmitting }) => (
