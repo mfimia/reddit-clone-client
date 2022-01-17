ab30bc86c2dec30577aade11bb5bc30fa95e14a7
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-17 15:21:10 +0100

    set up navbar component. login and logout links

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+// import Link from "next/link";
+import React, { Fragment } from "react";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  console.log(data);
+  let body = null;
+  // data is loading
+  if (fetching) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <Fragment>
+        <NextLink href={"/login"}>
+          <Link color={"white"} mr={2} href={""}>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href={"/register"}>
+          <Link color={"white"} href={""}>
+            Register
+          </Link>
+        </NextLink>
+      </Fragment>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant={"link"}>Logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex p={4} bg="tomato">
+      <Box ml="auto">{body}</Box>
+    </Flex>
+  );
+};
