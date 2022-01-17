e5d76bf6c0bbbde8e672aa8ba3636072d488fb67
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-17 10:54:27 +0100

    converts array into object

---
+++ src/utils/toErrorMap.ts
@@ -0,0 +1,11 @@
+import { FieldError } from "../generated/graphql";
+
+// Utility function that converts array into an object
+export const toErrorMap = (errors: FieldError[]) => {
+  const errorMap: Record<string, string> = {};
+  errors.forEach(({ field, message }) => {
+    errorMap[field] = message;
+  });
+
+  return errorMap;
+};
