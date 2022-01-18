2df1812c812f8a8d66768480a2f71d4b1fd37fe9
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-18 19:29:59 +0100

    moved client to separate folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
+import { withUrqlClient } from "next-urql";
 import { Fragment } from "react";
 import { NavBar } from "../components/NavBar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-  <Fragment>
-    <NavBar />
-  </Fragment>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <Fragment>
+      <NavBar />
+      {!data ? (
+        <div>Loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </Fragment>
+  );
+};
 
-export default Index;
+// enable server side rendering to improve SEO
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
