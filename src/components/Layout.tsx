b8781e835097d8fdcf7a2ff5826f6d9a051c38f9
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-24 18:28:50 +0100

    added wrapper component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import { NavBar } from "./NavBar";
+import { Wrapper, WrapperVariant } from "./Wrapper";
+
+interface LayoutProps {
+  variant?: WrapperVariant;
+}
+
+export const Layout: React.FC<LayoutProps> = ({
+  children,
+  variant = "regular",
+}) => {
+  return (
+    <Fragment>
+      <NavBar />
+      <Wrapper variant={variant}>{children}</Wrapper>
+    </Fragment>
+  );
+};
