c02e17dd0622e4ea0436def6dc9b45688033bde2
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-26 16:01:29 +0100

    rendering body post message input as text area

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,29 +3,43 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
+  ComponentWithAs,
+  InputProps,
+  TextareaProps,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 import { InputHTMLAttributes } from "react";
 
 // This input field takes the same props as any regular input field would take
-type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+type InputFieldProps = InputHTMLAttributes<
+  HTMLInputElement | HTMLTextAreaElement
+> & {
   // Declaring properties of the input and their types
   name: string;
   label: string;
+  textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   // Destructuring size to avoid errors. renaming it to underscore to make explicit that we wont be using it
   size: _,
   ...props
 }) => {
+  let InputOrTextarea:
+    | ComponentWithAs<"input", InputProps>
+    | ComponentWithAs<"textarea", TextareaProps> = Input;
+  if (textarea) {
+    InputOrTextarea = Textarea;
+  }
   const [field, { error }] = useField(props);
   return (
     // isInvalid takes a boolean so we cast error to boolean
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input
+      <InputOrTextarea
         {...props}
         {...field}
         id={field.name}
