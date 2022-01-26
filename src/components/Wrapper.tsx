bbd78b521dbcb1415992a490e5447801b8c298c1
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-26 16:02:15 +0100

    create wrapper component, optional sizes

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 
+export type WrapperVariant = "small" | "regular";
+
 interface WrapperProps {
   // Declaring props and possible values
-  variant?: "small" | "regular";
+  variant?: WrapperVariant;
 }
 
 export const Wrapper: React.FC<WrapperProps> = ({
